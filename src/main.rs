b36c33f12a5858b8bf04e3b4d9baa8ae5957e3c6
Akira Kano <a.kano1101@gmail.com>
2023-05-19 09:50:16 +0900

    id_tokenの取得ができるように。

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,4 @@
 use aws_sdk_cognitoidentityprovider as provider;
-use serde::{Deserialize, Serialize};
-use srp::SRP;
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[allow(non_snake_case)]
-struct ChallengeParameters {
-    // SESSION: String, // 独自追加
-    SALT: String,
-    SRP_B: String,
-    SECRET_BLOCK: String,
-    USER_ID_FOR_SRP: String,
-    USERNAME: String,
-}
-
-async fn request_challenge() -> anyhow::Result<ChallengeParameters> {
-    tracing::trace!("request_challengeに入りました。");
-    eprintln!("request_challengeに入りました。");
-
-    let url = "https://533sdf18m7.execute-api.ap-northeast-1.amazonaws.com/dev/auth";
-    let response = reqwest::get(url).await?;
-    tracing::trace!("reqwestのGETメソッドでSecondTestのURLにアクセスしました。");
-    eprintln!("{:?}", response);
-
-    // 認証情報をパース
-    let challenge_parameters_result = response.json().await;
-    eprintln!("{:?}", challenge_parameters_result);
-    let challenge_parameters = challenge_parameters_result?;
-    tracing::trace!("responseをjsonパースしました。");
-    // eprintln!("responseをjsonパースしました。");
-
-    Ok(challenge_parameters)
-    // unimplemented!()
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -48,59 +15,46 @@ async fn main() -> anyhow::Result<()> {
     let client = provider::Client::new(&config);
 
     let (secret_key, client_id, user_pool_id) = get_auth_info_from_env().await?;
-    tracing::info!("get_auth_info_from_envを実行しました。");
 
-    let username = "Akira";
-    let secret_hash = {
-        use aws_get_secret_value::get_secret_hash;
-        get_secret_hash(&username, &secret_key, &client_id)?
-    };
-    tracing::info!("シークレットハッシュを取得しました。");
+    let username = "add5a600-b08c-49fb-ad20-693fd0194eda";
+    // let username = "a.kano1101@gmail.com"; これはダメ
+    let password = "braQfuqVWtMCNcP6k-2o".to_string();
 
-    let challenge_parameters = request_challenge().await?;
-    tracing::info!("challengeを取得しました。");
-
-    let ChallengeParameters {
-        // SESSION: session,
-        SALT: salt,
-        SRP_B: srp_b,
-        SECRET_BLOCK: secret_block,
-        USER_ID_FOR_SRP: user_id_for_srp,
-        // USERNAME: username,
-        ..
-    } = challenge_parameters.clone();
-
-    let srp_client = SRP::new(user_pool_id);
-    tracing::info!("srp_clientを生成しました。");
-
-    let srp_a = srp_client.calculate_A().to_string();
-    let password = "".to_string();
-
-    let hkdf = srp_client.get_authenticate_key(
-        &user_id_for_srp,
+    let srp_client = cognito_srp::SrpClient::new(
+        &username,
         &password,
-        &srp_a,
-        &srp_b,
-        salt.as_bytes(),
+        &user_pool_id,
+        &client_id,
+        Some(&secret_key),
     );
-    let (timestamp, signature) = srp_client.sign(&hkdf, &user_id_for_srp, &secret_block);
-    tracing::info!("署名を取得しました。");
+
+    let initiate_auth_response = client
+        .initiate_auth()
+        .auth_flow(provider::types::AuthFlowType::UserSrpAuth)
+        .client_id(client_id.clone())
+        .set_auth_parameters(Some(srp_client.get_auth_params().unwrap()))
+        .send()
+        .await?;
+
+    let challenge_params = initiate_auth_response
+        .challenge_parameters
+        .ok_or(anyhow::anyhow!("failed to get challenge parameters"))?;
+
+    let challenge_responses = srp_client.process_challenge(challenge_params)?;
 
     use aws_sdk_cognitoidentityprovider::types::ChallengeNameType;
     let respond = client
         .respond_to_auth_challenge()
         .client_id(client_id)
         .challenge_name(ChallengeNameType::PasswordVerifier)
-        // .session(session)
-        .challenge_responses("TIMESTAMP", timestamp)
-        .challenge_responses("USERNAME", user_id_for_srp)
-        .challenge_responses("PASSWORD_CLAIM_SECRET_BLOCK", secret_block)
-        .challenge_responses("PASSWORD_CLAIM_SIGNATURE", signature)
-        .challenge_responses("SECRET_HASH", secret_hash)
+        .set_challenge_responses(Some(challenge_responses))
         .send()
         .await?;
 
-    tracing::warn!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!{:?}", respond);
+    let authentication_result = respond.authentication_result.unwrap();
+    let id_token = authentication_result.id_token.unwrap();
+
+    tracing::warn!("id_token: {}", id_token);
 
     Ok(())
 }
